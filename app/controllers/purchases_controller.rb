28774ac79638f333e90a3911415f54a9af8cfa77
ghostly08 <steven.produccionbcn@gmail.com>
2023-07-12 21:51:40 +0200

    fix bug on purchase_at attribute

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,17 +6,22 @@ class PurchasesController < ApplicationController
   end
 
   def create
-    # @user = User.find(params[:user_id])
     @user = User.find(1)
 
     @purchase = @user.purchases.create(purchase_params)
-    render json: @purchase
+    @purchase.purchased_at = Time.now
+
+    if @purchase.save
+      render json: @purchase
+    else
+      render json: @purchase.errors, status: :unprocessable_entity
+    end
   end
 
   private
 
   def purchase_params
-    params.require(:purchase).permit(:content_id, :content_type, :quality, :price, :purchase_at)
+    params.require(:purchase).permit(:content_id, :content_type, :quality, :price, :purchased_at)
   end
 
 end
